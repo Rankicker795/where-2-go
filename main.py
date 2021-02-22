76045ac91d6faf0eff663f513c6cc4acf401d5ae
Roger Allen <roger.allen795@gmail.com>
2021-02-21 19:56:16 -0600

    Created main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from itertools import chain
+from random import randint
+
+from restconfig import dinner, brunch
+
+
+def _combine_lists(*args):
+    return set(chain(*args))
+
+
+def generate_rest_dict(choice: str) -> dict:
+    restaurants = {
+        "dinner": set(dinner),
+        "brunch": set(brunch),
+        "all": _combine_lists(dinner, brunch),
+    }
+    return {index: value for index, value in enumerate(restaurants.get(
+        choice))}
+
+
+def make_random_choice(dict_) -> str:
+    r_int = randint(0, len(dict_.keys()) - 1)
+    return dict_.get(r_int)
+
+
+if __name__ == "__main__":
+    d = generate_rest_dict(input("Dinner, Brunch, or All? ").strip().lower())
+    print(make_random_choice(d))
