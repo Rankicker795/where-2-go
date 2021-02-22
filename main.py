189ee3b2dc0256d11b73f5e921a7cd7d7233bc65
Roger Allen <roger.allen795@gmail.com>
2021-02-21 21:26:02 -0600

    fixed bug with flask

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request
 from where_2_go import *
 
 app = Flask(__name__)
@@ -9,8 +9,13 @@ def success(location):
     return f"where-2-go has chosen {location}"
 
 
-@app.route("/where-2-go", methods=["POST", "GET"])
+@app.route("/")
 def index():
+    return render_template("index.html")
+
+
+@app.route("/where_2_go", methods=["POST", "GET"])
+def where_2_go():
     if request.method == "POST":
         meal = request.form["meal"]
         dict_ = generate_rest_dict(meal.strip().lower())
